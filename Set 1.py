b618baed446690a8994a8d7675ba657c0b6c8b66
MarcosP7635 <maperez@caltech.edu>
2021-01-06 23:02:18 -0800

    saving progress

--- Set 1.py
+++ Set 1.py
@@ -1,1 +1,27 @@
-print("hello")
+import numpy as np
+import math
+import matplotlib as plt
+import matplotlib.pyplot as plt
+def calculateP (Ai, mu, sigma):
+    #sigma = 2
+#I think the problem specifies this value but I will have to check with someone later
+    p = np.log((1/(sigma*math.sqrt(2*np.pi))))+((-.5*((Ai-mu/sigma)**2)))
+    p = (p+760)/-300
+#This is the probability of obtaining a specific value of Ai given mu and sigma
+    return p
+print(calculateP(1, 1, 2))
+A1 = 41.4
+def makePlot(iterations, Ai, sigma):
+    muDistr = np.random.uniform(-3*sigma,3*sigma, iterations)
+    AiArray = [0]*iterations
+    for i in range(len(muDistr)):
+        mu = muDistr[i]
+        AiArray[i] = calculateP(Ai, mu, sigma)
+        #print(AiArray)
+    plt.scatter(muDistr,AiArray)
+    #plt.title('Probability of obtaining Ai = ', Ai
+    yaxis = "The natural log of the Probability of obtaining Ai = " + str(Ai)
+    plt.ylabel(yaxis)
+    plt.xlabel('mu')
+    plt.show()
+makePlot(10**5,A1,2)
