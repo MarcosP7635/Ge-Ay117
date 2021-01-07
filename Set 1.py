7a793830865b0abab6bd29f29e1040960025a9a9
MarcosP7635 <maperez@caltech.edu>
2021-01-07 00:14:17 -0800

    Normalized the natural log of the probability

--- Set 1.py
+++ Set 1.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 def calculateP (Ai, mu, sigma):
     #sigma = 2
 #I think the problem specifies this value but I will have to check with someone later
-    p = np.log((1/(sigma*math.sqrt(2*np.pi))))+((-.5*((Ai-mu/sigma)**2)))
-    p = (p+760)/-300
+    p = (((np.log((1/(sigma*math.sqrt(2*np.pi))))+((-.5*((Ai-mu/sigma)**2)))))+987.2907100867332)/(987.2907100867332-738.8943667164641)
+    #p = (np.e**((-.5*((Ai-mu/sigma)**2)+987.2907100867332)))
+    #p = p*((1/(sigma*math.sqrt(2*np.pi))))
+    #p = (p+760)/-300
 #This is the probability of obtaining a specific value of Ai given mu and sigma
     return p
 print(calculateP(1, 1, 2))
@@ -18,9 +20,11 @@ def makePlot(iterations, Ai, sigma):
         mu = muDistr[i]
         AiArray[i] = calculateP(Ai, mu, sigma)
         #print(AiArray)
+    print(np.amin(AiArray))
+    print(np.amax(AiArray))
     plt.scatter(muDistr,AiArray)
     #plt.title('Probability of obtaining Ai = ', Ai
-    yaxis = "The natural log of the Probability of obtaining Ai = " + str(Ai)
+    yaxis = "Normalized natural log of the Probability of obtaining Ai = " + str(Ai)
     plt.ylabel(yaxis)
     plt.xlabel('mu')
     plt.show()
