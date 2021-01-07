25735daf49d5a54cff3385de1e22fb0e62cbed12
MarcosP7635 <maperez@caltech.edu>
2021-01-07 01:10:11 -0800

    Noramlized probability mpmath now change mu range

--- Set 1.py
+++ Set 1.py
@@ -2,30 +2,45 @@ import numpy as np
 import math
 import matplotlib as plt
 import matplotlib.pyplot as plt
+from mpmath import*
 def calculateP (Ai, mu, sigma):
-    #sigma = 2
-#I think the problem specifies this value but I will have to check with someone later
-    p = (((np.log((1/(sigma*math.sqrt(2*np.pi))))+((-.5*((Ai-mu/sigma)**2)))))+987.2907100867332)/(987.2907100867332-738.8943667164641)
-    #p = (np.e**((-.5*((Ai-mu/sigma)**2)+987.2907100867332)))
-    #p = p*((1/(sigma*math.sqrt(2*np.pi))))
+    p = e**((-.5*((Ai-mu/sigma)**2)))
+    p = p*((1/(sigma*((2*pi)**.5))))
     #p = (p+760)/-300
 #This is the probability of obtaining a specific value of Ai given mu and sigma
     return p
 print(calculateP(1, 1, 2))
 A1 = 41.4
+#Value preset by the problem
 def makePlot(iterations, Ai, sigma):
-    muDistr = np.random.uniform(-3*sigma,3*sigma, iterations)
+    muDistr = np.random.uniform(50,100, iterations)
     AiArray = [0]*iterations
     for i in range(len(muDistr)):
         mu = muDistr[i]
         AiArray[i] = calculateP(Ai, mu, sigma)
         #print(AiArray)
-    print(np.amin(AiArray))
-    print(np.amax(AiArray))
+    min = np.amin(AiArray)
+    max = np.amax(AiArray)
     plt.scatter(muDistr,AiArray)
     #plt.title('Probability of obtaining Ai = ', Ai
-    yaxis = "Normalized natural log of the Probability of obtaining Ai = " + str(Ai)
+    yaxis = "Probability of obtaining Ai = " + str(Ai)
     plt.ylabel(yaxis)
     plt.xlabel('mu')
     plt.show()
+    for i in range(len(muDistr)):
+        mu = muDistr[i]
+        AiArray[i] = normalizeP(max,min, Ai, mu, sigma)
+    plt.scatter(muDistr,AiArray)
+    #plt.title('Probability of obtaining Ai = ', Ai
+    yaxis = "Normalized Probability of obtaining Ai = " + str(Ai)
+    plt.ylabel(yaxis)
+    plt.xlabel('mu')
+    plt.show()
+#This function calculates the normalized probability
+def normalizeP (max, min, Ai, mu, sigma):
+    p = e**((-.5*((Ai-mu/sigma)**2)))
+    p = p*((1/(sigma*((2*pi)**.5))))
+    p = (p-min)/(max-min)
+    return p
+#call everything make the plot
 makePlot(10**5,A1,2)
