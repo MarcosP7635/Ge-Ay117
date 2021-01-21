521428f2bac694fa8535ed84f38f42242e9dd65b
MarcosP7635 <maperez@caltech.edu>
2021-01-21 00:08:15 -0800

    made a script that outputs the dataset for kaden in problem 1 :)

---
+++ set 2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import math
+import matplotlib as plt
+import matplotlib.pyplot as plt
+from mpmath import*
+def calculateX (alpha, beta, smalln, bign):
+    x = [0]*smalln
+    feederX = [0]*bign
+    probs = [0]*bign
+    counts = np.empty(0)
+    '''
+    will randomly choose integers and make them the first row (0 to 100)
+    will compute their probabilities
+    '''
+    for i in range(bign):
+        feederX[i] = np.random.uniform(0, 100)
+        probs[i] = beta/(pi*(beta**2+(feederX[i]**2-alpha**2)))
+        count = [feederX[i]]*int(probs[i]*(bign))
+        counts = np.append(counts, count)
+#now I need to draw from this distribution.
+    for i in range(smalln):
+        index = int(np.random.uniform(0, len(counts)-1))
+        x[i] = counts[index]
+    return x
+print(calculateX(50, 2, 16, 10**4))
